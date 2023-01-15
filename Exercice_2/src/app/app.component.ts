ba663b01c6831003a7ba2cfa06b686c9602c3168
akouvivien <akouvivien@gmail.com>
2023-01-15 14:54:45 +0000

    first commit exo xp day4

--- Exercice_2/src/app/app.component.ts
+++ Exercice_2/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DI_Bootcamp_Week4-Day4_ExercicesXP';
+  title = 'Exercice_2';
 }
--- Exercice_2/src/app/app.module.ts
+++ Exercice_2/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ColorFormComponent } from './color-form-component/color-form-component.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColorFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
